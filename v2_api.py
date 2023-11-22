2a723359c9d219cbd63799bc7168070e0977f958
XBOPb <redthunder458@yandex.ru>
2023-11-22 08:08:01 +0300

    url scanner

--- v2_api.py
+++ v2_api.py
@@ -5,7 +5,7 @@ file_path = r''
 api_key = ''
 params = dict(apikey=api_key)
 
-def send_to_check():
+def check_file():
     scan_url = 'https://virustotal.com/vtapi/v2/file/scan'
     with open(file_path, 'rb') as file:
         files = dict(file=(file_path, file))
@@ -17,7 +17,7 @@ def send_to_check():
         scan_id = json_dict.get('scan_id')
         return scan_id
     
-def get_scan_result(scan_id):
+def file_check_result(scan_id):
     report_url = 'https://virustotal.com/vtapi/v2/file/report'
     params = dict(apikey=api_key, resource=scan_id)
     response = requests.get(report_url, params=params)
@@ -26,6 +26,28 @@ def get_scan_result(scan_id):
         print(json.dumps(result, sort_keys=False, indent=4))
 
 
+def check_link(link):
+    scan_url = 'https://virustotal.com/vtapi/v2/url/scan'
+    params = dict(apikey=api_key, url=link)
+    response = requests.post(scan_url, data=params)
+    if response.status_code == 200:
+        result = response.json()
+        json_result = json.dumps(result, sort_keys=False, indent=4)
+        json_dict = json.loads(json_result)
+        scan_id = json_dict.get('scan_id')
+        return scan_id
+
+
+def link_check_result(scan_id):
+    report_url = 'https://virustotal.com/vtapi/v2/file/report'
+    params = dict(apikey=api_key, resource=scan_id)
+    response = requests.get(report_url, params=params)
+    if response.status_code == 200:
+        result = response.json()
+        print(json.dumps(result, sort_keys=False, indent=4))
+
 if __name__ == '__main__':
-    scan_id = send_to_check()
-    get_scan_result(scan_id)
+    scan = check_link('https://google.com')
+    link_check_result(scan)
+    scan_id = check_file()
+    file_check_result(scan_id)
